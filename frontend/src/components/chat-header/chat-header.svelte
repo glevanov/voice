bb800c47202683fc9289c3703f526346b972f565
Grigoriy Levanov <gregory@levanov.io>
2025-09-30 22:20:20 +0200

    restyle clear chat button

--- frontend/src/components/chat-header/chat-header.svelte
+++ frontend/src/components/chat-header/chat-header.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
-  import { clearMessages } from "../../store/messages";
   import StatusIndicator from "../status-indicator/status-indicator.svelte";
+  import ClearChat from "../clear-chat/clear-chat.svelte";
 </script>
 
 <div class="chat-header">
   <StatusIndicator />
-  <button on:click={clearMessages} class="clear">Clear History</button>
+  <ClearChat />
 </div>
 
 <style>
   .chat-header {
     display: flex;
     justify-content: space-between;
-  }
-
-  .clear {
-    padding: 5px 10px;
-    background-color: #dc3545;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 0.9em;
-  }
-
-  .clear:hover {
-    background-color: #c82333;
   }
 </style>
