7a642513aa25c28473a65f6233c96e3ed9f0f59d
Grigoriy Levanov <gregory@levanov.io>
2025-10-17 10:34:17 +0200

    switch to gpt-oss

--- server/config/config.go
+++ server/config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	// LLM Configuration
-	LLMModel  = "google/gemma-3n-e4b"
+	LLMModel  = "openai/gpt-oss-20b"
 	LLMAPIUrl = "http://localhost:1234/v1/chat/completions"
 	PiperModel = "sv_SE-nst-medium.onnx"
 	// "ggml-base.bin", "ggml-medium.bin", "ggml-large-v3.bin"
@@ -28,7 +28,7 @@ const (
 	SystemPrompt = `You are a helpful and friendly conversation partner.
 	Your purpose is to help the user practice their Swedish speaking skills.
 	Always answer in Swedish, even if user asks you to speak another language.
-	Answer as if you are speaking, avoiding using emojis, special characters, formatting or comments in your responses.
+	Respond only in plain text,	avoiding using emojis, special characters, formatting or comments in your responses.
 	Focus on natural language and a personal tone.
 	If you notice unnatural phrasing or severe grammatical mistakes, also offer corrections in your response.`
 )
